1e1cc70c9c4809fe6f4f96a47d5bed600f0cd68e
GuYuLong <guyulong88888@qq.com>
2022-11-13 21:13:25 +0800

    新增python批量加执行脚本

---
+++ chmodx.sh
@@ -0,0 +1,4 @@
+chmod +x ./ros_ws/src/vehicle_motherboard/src/*.py
+chmod +x ./ros_ws/src/vehicle_mission/script/*.py
+chmod +x ./ros_ws/src/vehicle/script/*.sh
+chmod +x ./ros_ws/src/vehicle/script/*.py
